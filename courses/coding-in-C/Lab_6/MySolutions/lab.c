19beca235ecb0f82d86b777151bc368deb6c2f2b
batu <oezdemir.batu-it25@it.dhbw-ravensburg.de>
2026-02-18 14:00:06 +0100

    lab 6

---
+++ courses/coding-in-C/Lab_6/MySolutions/lab.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+int main(void) {
+    FILE *f = fopen("test.txt", "r");
+    //FILE *f2 = fopen("test.txt", "w");
+    
+    if (f == NULL) {
+        printf("Fehler beim Öffnen der Datei.\n");
+        return 1;
+    }
+    /*int c = fgetc(f);
+    printf("Dateiinhalt 1 Zeichen: %c\n", c);
+    char s[100];
+    fgets(s, 100, f);
+    printf("Dateiinhalt 1 Zeile: %s\n", s);
+    fclose(f);
+    return 0;
+    */
+   char p[100];
+    fscanf(f, "%s", p);
+    printf("Dateiinhalt 1 Wort: %s\n", p);
+    fclose(f);  
+
+
+}
